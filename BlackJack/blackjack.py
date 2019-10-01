4d4e1b24aa4f37a835f7a69533199fce8043c5da
arthurshmidt <47873761+arthurshmidt@users.noreply.github.com>
2019-10-01 18:53:51 -0400

    Outlined class init and functions

--- BlackJack/blackjack.py
+++ BlackJack/blackjack.py
@@ -6,12 +6,61 @@ class bj_deck():
     Enter description, attributes and functions of the class.
     """
 
-    pass
+    def __init__(self):
+        """
+        Enter description
+        """
+        self.deck = {}
+        self.deck_shuffle()
 
+    def deck_shuffle(self):
+        """
+        Enter description
+        """
+        pass
+
+    def draw_card(self):
+        """
+        Enter description
+        """
+        pass
 
 class bj_player():
     """
     Enter description, attributes and functions of the class.
     """
 
-    pass
+    def __init__(self,player_name="Computer",is_computer=True,chips=0):
+        """
+        Enter description
+        """
+
+        self.player_name = player_name
+        self.is_computer = is_computer
+        self.chips = chips
+        self.cards = []                 # list of dictionaries
+        self.bet = 0
+
+    def add_chips(self,chips):
+        """
+        Enter description
+        """
+        pass
+
+    def add_cards(self,cards):
+        """
+        Enter description
+        """
+        pass
+
+    def place_bet(self):
+        """
+        Enter description
+        """
+        pass
+
+    def card_count(self):
+        """
+        Enter description
+        """
+        pass
