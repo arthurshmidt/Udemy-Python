3d0db5f3a33f549f03192d949c385b04e0dcfed3
arthurshmidt <47873761+arthurshmidt@users.noreply.github.com>
2019-10-01 14:03:27 -0400

    Created package directory, init, and class outline.

---
+++ BlackJack/blackjack.py
@@ -0,0 +1,17 @@
+# blackjack.py is a python file hold all the classes necessary for the
+# blackjack program.
+
+class bj_deck():
+    """
+    Enter description, attributes and functions of the class.
+    """
+
+    pass
+
+
+class bj_player():
+    """
+    Enter description, attributes and functions of the class.
+    """
+
+    pass
